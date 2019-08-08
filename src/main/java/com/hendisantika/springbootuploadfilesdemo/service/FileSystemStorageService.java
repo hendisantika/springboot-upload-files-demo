f295402ec62fedd15a97524bc2349b84fe491a57
Hendi Santika <hendisantika@gmail.com>
2019-08-09 05:41:42 +0700

    #12 - Add loadAsResource in FileSystemStorageService

--- src/main/java/com/hendisantika/springbootuploadfilesdemo/service/FileSystemStorageService.java
+++ src/main/java/com/hendisantika/springbootuploadfilesdemo/service/FileSystemStorageService.java
@@ -1,14 +1,19 @@
 package com.hendisantika.springbootuploadfilesdemo.service;
 
+import com.hendisantika.springbootuploadfilesdemo.exception.FileNotFoundException;
 import com.hendisantika.springbootuploadfilesdemo.exception.StorageException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -83,5 +88,26 @@ public class FileSystemStorageService implements StorageService {
     @Override
     public Path load(String filename) {
         return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException(
+                        "Could not read file: " + filename);
+            }
+        } catch (MalformedURLException e) {
+            throw new FileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
     }
 }
