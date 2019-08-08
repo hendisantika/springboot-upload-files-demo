e1fe949d62eb9260d6014235213a24963e5c475d
Hendi Santika <hendisantika@gmail.com>
2019-08-09 05:43:55 +0700

    #16 - Add uploadFile in FileController

--- src/main/java/com/hendisantika/springbootuploadfilesdemo/controller/FileController.java
+++ src/main/java/com/hendisantika/springbootuploadfilesdemo/controller/FileController.java
@@ -1,14 +1,14 @@
 package com.hendisantika.springbootuploadfilesdemo.controller;
 
+import com.hendisantika.springbootuploadfilesdemo.common.FileResponse;
 import com.hendisantika.springbootuploadfilesdemo.service.StorageService;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.stream.Collectors;
@@ -54,5 +54,18 @@ public class FileController {
                 .header(HttpHeaders.CONTENT_DISPOSITION,
                         "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
+    }
+
+    @PostMapping("/upload-file")
+    @ResponseBody
+    public FileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String name = storageService.store(file);
+
+        String uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/download/")
+                .path(name)
+                .toUriString();
+
+        return new FileResponse(name, uri, file.getContentType(), file.getSize());
     }
 }
