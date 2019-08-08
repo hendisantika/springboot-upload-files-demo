b4fc5086a2bd8966df260827dc7703203a94db20
Hendi Santika <hendisantika@gmail.com>
2019-08-09 05:37:37 +0700

    #4 - Add StorageException

---
+++ src/main/java/com/hendisantika/springbootuploadfilesdemo/exception/StorageException.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootuploadfilesdemo.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-upload-files-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-09
+ * Time: 05:37
+ */
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
