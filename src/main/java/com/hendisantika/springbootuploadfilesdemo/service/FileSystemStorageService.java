c0f2dd2dee0b2864fc2ba90fc71168904c046c8c
Hendi Santika <hendisantika@gmail.com>
2019-08-09 05:40:27 +0700

    #10 - Add store in FileSystemStorageService

--- src/main/java/com/hendisantika/springbootuploadfilesdemo/service/FileSystemStorageService.java
+++ src/main/java/com/hendisantika/springbootuploadfilesdemo/service/FileSystemStorageService.java
@@ -3,12 +3,16 @@ package com.hendisantika.springbootuploadfilesdemo.service;
 import com.hendisantika.springbootuploadfilesdemo.exception.StorageException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +41,29 @@ public class FileSystemStorageService implements StorageService {
         } catch (IOException e) {
             throw new StorageException("Could not initialize storage location", e);
         }
+    }
+
+    @Override
+    public String store(MultipartFile file) {
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + filename);
+            }
+            if (filename.contains("..")) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file with relative path outside current directory "
+                                + filename);
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, this.rootLocation.resolve(filename),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file " + filename, e);
+        }
+
+        return filename;
     }
 }
