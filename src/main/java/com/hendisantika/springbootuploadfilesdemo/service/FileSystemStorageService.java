4cb0e3600547d8a1f1d89e3c79d3116f8ae1464c
Hendi Santika <hendisantika@gmail.com>
2019-08-09 05:40:05 +0700

    #9 - Add init in FileSystemStorageService

--- src/main/java/com/hendisantika/springbootuploadfilesdemo/service/FileSystemStorageService.java
+++ src/main/java/com/hendisantika/springbootuploadfilesdemo/service/FileSystemStorageService.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springbootuploadfilesdemo.service;
 
+import com.hendisantika.springbootuploadfilesdemo.exception.StorageException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -23,5 +27,15 @@ public class FileSystemStorageService implements StorageService {
     @Autowired
     public FileSystemStorageService(StorageProperties properties) {
         this.rootLocation = Paths.get(properties.getLocation());
+    }
+
+    @Override
+    @PostConstruct
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage location", e);
+        }
     }
 }
